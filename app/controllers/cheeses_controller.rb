842178814e3e621fcc4f2d77e67991991092e088
Tressie Muthuri <tressiemuthuri@gmail.com>
2022-11-21 15:17:30 +0300

    Completed assignment

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -5,4 +5,12 @@ class CheesesController < ApplicationController
     render json: cheeses
   end
 
+  def show
+    #find a cheese using the id from the url
+    cheese = Cheese.find_by(id: params[:id])
+    # send a json response using that cheese object
+    render json: cheese
+    # byebug
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   get '/cheeses', to: 'cheeses#index'
+  get '/cheeses/:id', to: 'cheeses#show'
+  # get '/cheeses/2', to: 'cheeses#second'
+
 end
